10652a09d0606df02902869cf7e7530d53ff8e55
Jakub Lewandowski <s16651@pjwstk.edu.pl>
2020-03-05 16:56:27 +0100

    initial commit

---
+++ solution/tutorial1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace tutorial1
+{
+    public class Program
+    {
+       public static async Task Main(string[] args)
+        {
+            var websiteurl = args[0];
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteurl);
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlcontent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(htmlcontent);
+                foreach (var emailAdress in matches)
+                {
+                    Console.WriteLine(emailAdress);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
